5298c3ac2979edcab8b48d7315d8bef36c473902
SNOOR <noor.sm@epa.gov>
2026-06-30 11:29:36 -0400

    CE-5122: dormatting the code segment.

--- src/main/java/gov/epa/ccte/api/exposure/web/rest/error/HigherNumberOfDtxsidException.java
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/error/HigherNumberOfDtxsidException.java
@@ -1,9 +1,8 @@
 package gov.epa.ccte.api.exposure.web.rest.error;
 
-
 public class HigherNumberOfDtxsidException extends RuntimeException {
-    public HigherNumberOfDtxsidException(Integer size, Integer maxSize){
-        super(String.format("System supports only '%s' dtxsid at one time, '%s' are submitted.",maxSize, size));
+	public HigherNumberOfDtxsidException(Integer size, Integer maxSize) {
+		super(String.format("System supports only '%s' dtxsid at one time, '%s' are submitted.", maxSize, size));
 
-    }
+	}
 }
