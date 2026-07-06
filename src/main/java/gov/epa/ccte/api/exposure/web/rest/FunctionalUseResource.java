1be4a9df941406df7a3dcc7c6b266a201b46023b
SNOOR <noor.sm@epa.gov>
2026-07-06 10:40:21 -0400

    CE-5122: added app.batch.size instead of exposure.batch.size.

--- src/main/java/gov/epa/ccte/api/exposure/web/rest/FunctionalUseResource.java
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/FunctionalUseResource.java
@@ -23,7 +23,7 @@ public class FunctionalUseResource implements FunctionalUseApi {
         private final FunctionalUseCategoryRepository categoryRepository;
         private final QsurDataRepository qsurDataRepository;
         
-        @Value("${exposure.batch.size}")
+        @Value("${app.batch.size}")
         private Integer batchSize;
 
     public FunctionalUseResource(FunctionalUseRepository repository, FunctionalUseCategoryRepository categoryRepository, QsurDataRepository qsurDataRepository) {
