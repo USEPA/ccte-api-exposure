68359a8130cefc34a211d42dfdd137a93133c4ff
SrikanthDeevi <deevi.srikanth@epa.gov>
2026-07-24 10:25:19 -0400

    CE-5279- Fixed test cases to use in memory db instead of test containers

--- src/test/java/gov/epa/ccte/api/exposure/repository/HttkDataRepositoryTest.java
+++ src/test/java/gov/epa/ccte/api/exposure/repository/HttkDataRepositoryTest.java
@@ -4,50 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import gov.epa.ccte.api.exposure.domain.HttkData;
-
-import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 @Sql(scripts = {"/schema.sql", "/data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Testcontainers
-@DataJpaTest
-@ActiveProfiles("test")
+@DataJpaTest(properties = "spring.sql.init.mode=never")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class HttkDataRepositoryTest {
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> pgsqldb = new PostgreSQLContainer<>("postgres:16-alpine");
 
-    @Autowired
-    private DataSource dataSource;
-    @Autowired private JdbcTemplate jdbcTemplate;
-    @Autowired private TestEntityManager entityManager;
     @Autowired private HttkDataRepository repository;
-
-    @Test
-    void connectionEstablished(){
-        assertThat(pgsqldb.isCreated()).isTrue();
-        assertThat(pgsqldb.isRunning()).isTrue();
-    }
-
-    @Test
-    void injectedComponentsAreNotNull() {
-        assertThat(dataSource).isNotNull();
-        assertThat(jdbcTemplate).isNotNull();
-        assertThat(entityManager).isNotNull();
-        assertThat(repository).isNotNull();
-    }
 
     // Now test data loaded or not
     @Test
@@ -56,17 +26,15 @@ class HttkDataRepositoryTest {
     }
 
     @Test
-    void testHttkDataByDtxsid() { 
-    	assertThat(repository.findByDtxsid("DTXSID7020182")).size().isEqualTo(5);
-        
-    	assertThat(repository.findByDtxsid("DTXSID9020112")).size().isEqualTo(5);
-    }
-    
-    @Test
-    void testHttkDataByBatchDtxsid() {
-    	String[] dtxsids = {"DTXSID7020182","DTXSID9020112"};
-    	assertThat(repository.findByDtxsidInOrderByDtxsidAsc(dtxsids, HttkData.class)).size().isEqualTo(10);
-        
+    void testHttkDataByDtxsid() {
+        assertThat(repository.findByDtxsid("DTXSID7020182")).size().isEqualTo(5);
+
+        assertThat(repository.findByDtxsid("DTXSID9020112")).size().isEqualTo(5);
     }
 
+    @Test
+    void testHttkDataByBatchDtxsid() {
+        String[] dtxsids = {"DTXSID7020182","DTXSID9020112"};
+        assertThat(repository.findByDtxsidInOrderByDtxsidAsc(dtxsids, HttkData.class)).size().isEqualTo(10);
+    }
 }
