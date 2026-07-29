aaa17a429c4777beee5334f570a17c92ea6161e4
SrikanthDeevi <62434984+SrikanthDeevi@users.noreply.github.com>
2026-07-29 14:31:08 -0400

    CE-5279- Fixed test cases to use in memory db instead of test containers (#172)
    * Fixed test cases to use in memory db instead of test containers
    * Addressed copilot comments to stabilize in-memory test setup and SQL init transactions
    * fixed functional spelling
    * removed test container dependencies and junit 4 annotations

--- src/test/java/gov/epa/ccte/api/exposure/web/rest/ListPresenceResourceTest.java
+++ src/test/java/gov/epa/ccte/api/exposure/web/rest/ListPresenceResourceTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 //This will test REST end-points in the ListPresenceResource.java using WebMvcTest and MockitoBean
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -32,7 +30,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ActiveProfiles("test")
 @WebMvcTest({ListPresenceResource.class})
-@RunWith(MockitoJUnitRunner.class)
 class ListPresenceResourceTest {
 
     @Autowired
